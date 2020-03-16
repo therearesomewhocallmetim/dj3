6c7720b606bfb2c356b683000a7dd9b2b0ae6655
Timofey Danshin <t.danshin@gmail.com>
2020-03-16 16:58:41 +0300

    Filter with conditional prefetch

--- filter_related_through/models.py
+++ filter_related_through/models.py
@@ -1,13 +1,30 @@
+from datetime import datetime
+
 from django.contrib.postgres.fields import DateTimeRangeField
 from django.db import models
+from django.db.models import Prefetch
+
+
+class UserQuerySet(models.QuerySet):
+    def with_professions(self):
+        pref = Prefetch('professions', queryset=UserProfession.objects.current())
+        return self.prefetch_related(pref)
+
 
 class User(models.Model):
     name = models.CharField(max_length=500)
+
+    objects = UserQuerySet.as_manager()
 
 
 class Profession(models.Model):
     name = models.CharField(max_length=500)
     users = models.ManyToManyField(User, through='UserProfession')
+
+
+class UserProfessionQuerySet(models.QuerySet):
+    def current(self):
+        return self.filter(period__contains=datetime.now()).select_related('profession')
 
 
 class UserProfession(models.Model):
@@ -18,7 +35,9 @@ class UserProfession(models.Model):
         Profession, blank=True, null=True, on_delete=models.CASCADE)
     period = DateTimeRangeField()
 
-# FilteredRelation
-# ringo = User.objects.create(name='Ringo Starr')
-# drummer = Profession.objects.create(name='drummer')
-# drummer.users.add(ringo, through_defaults={"period": DateTimeTZRange('2020-01-01')})
+    objects = UserProfessionQuerySet.as_manager()
+
+
+def the_fn():
+    rng = User.objects.with_professions().get(pk=1)
+    print(f'ringo is a {[x.profession.name for x in rng.professions.all()]}')
